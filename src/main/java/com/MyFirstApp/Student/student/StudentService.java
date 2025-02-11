b5a4850d47519264795a39b395c64f5259485f47
Tshembho326 <xvmrhu001@myuct.ac.za>
2025-02-11 13:47:34 +0200

    Configured PostgreSQL with my Springboot app, also injected a repo to my services

--- src/main/java/com/MyFirstApp/Student/student/StudentService.java
+++ src/main/java/com/MyFirstApp/Student/student/StudentService.java
@@ -1,16 +1,21 @@
 package com.MyFirstApp.Student.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
     @GetMapping
     public List<Student> getStudent() {
-        return List.of(new Student(1L,21, "xivambuntshembo@gmail.com ",
-                "Tshembho326", LocalDate.of(2003, 10, 12)));
+        return studentRepository.findAll();
     }
 }
